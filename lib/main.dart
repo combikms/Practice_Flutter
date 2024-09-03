91c4283aad27e48e58d16fbfbf6e1ff8df4b5b80
combikms <combikms@naver.com>
2024-09-03 18:01:49 +0900

    :sparkles: stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,82 +4,69 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a = 1;
+  var name = ['김길산', '박등주', '차일두', '피자집'];
+  var like = [13, 7, 2, 54];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Text(a.toString()),
+          onPressed: (){
+            setState(() {
+              a += 1;
+            });
+          },
+        ),
         appBar: AppBar(
-          leading: Icon(Icons.menu), title: Text('CalGPT'),
+          leading: Icon(Icons.menu), title: Text('ConGPT'),
           titleTextStyle: TextStyle(
             fontSize: 20,
           ),
         ),
-
-
-        body: Container(
-          width: double.infinity, height: 200,
-            padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Container(
-                width: 150,
-                child: Image.asset('assets/dog.png'),
-              ),
-              Container(
-                width: 380,
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'iPhone 15 Pro Max 1TB',
-                      style: TextStyle(
-                        fontSize: 25
-                      ),
-                    ),
-                    Text(
-                      '구미시 양호동 | 끌올 10분전',
-                      style: TextStyle(
-                        color: Colors.black38
-                      )
-                    ),
-                    Text(
-                      '1,500,000원',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w700
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(' ♡ ', style: TextStyle(fontSize: 25)),
-                        Text('4', style: TextStyle(fontSize: 20)),
-                      ],
-                    )
-                  ],
-                )
-              )
-            ],
-          )
-
-
+        body: ListView.builder(
+          itemCount: name.length,
+            itemBuilder: (c, i) {
+              return ListTile(
+                leading: Text(like[i].toString(), style: TextStyle(fontSize: 15),),
+                title: Text(name[i]),
+                trailing: TextButton(onPressed: () {
+                  setState(() {
+                    like[i] += 1;
+                  });
+                }, child: Text('좋아요'))
+              );
+            }
         ),
-
         bottomNavigationBar: BottomAppBar(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Icon(Icons.phone),
-              Icon(Icons.message),
-              Icon(Icons.contact_page),
-            ],
-          )
+          child: NavBar(),
         ),
       )
+    );
+  }
+}
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(Icons.phone),
+        Icon(Icons.message),
+        Icon(Icons.contact_page),
+      ],
     );
   }
 }
